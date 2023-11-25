e9c50b2f181cdd0f86d4aa5f2829ef761267ab75
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-11-25 20:02:48 +0500

    login ki ui bhi hogai hai

--- src/components/Login/App.jsx
+++ src/components/Login/App.jsx
@@ -1,8 +1,39 @@
-function App(){
+import './App.css'
+
+function App() {
     return (
-        <>
-            Login page hai samjhai
-        </>
+        <div className='signuppage'>
+            <div className="signupcard">
+                <div className='signUpOrSignInDiv'>
+                    <button className='acitvebtn'>
+                        <span><i className="fa-solid fa-user"></i></span>
+                        <span>Sign In</span>
+                    </button>
+                    <button>
+                        <span><i className="fa-solid fa-user-plus"></i></span>
+                        <span>Sign Up</span>
+                    </button>
+                </div>
+                <div className='inputDivSignup UserNameInputDiv'>
+                    <i className="fa-regular fa-circle-user"></i>
+                    <input placeholder='Username' type="text" />
+                </div>
+                <div className='inputDivSignup PassworInputDiv'>
+                    <i className="fa-solid fa-keyboard"></i>
+                    <input placeholder='Password' type="password" />
+                </div>
+                <div>
+                    <span style={{ color: '#6f11f5', cursor: 'pointer', textDecoration: 'underline' }}>Forget Password?</span>
+                </div>
+                <div className='my-3'>
+                    <input className="form-check-input me-1 ms-3" checked type="checkbox" value="" id="flexCheckDefault" />
+                    <span>Remember Me</span>
+                </div>
+                <div className='SignUpBtn mt-2'>
+                    <button><i className="fa-solid fa-user-plus me-1"></i> Sign up</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Signup/App.jsx
+++ src/components/Signup/App.jsx
@@ -6,35 +6,35 @@ function App() {
             <div className="signupcard">
                 <div className='signUpOrSignInDiv'>
                     <button>
-                        <span><i class="fa-solid fa-user"></i></span>
+                        <span><i className="fa-solid fa-user"></i></span>
                         <span>Sign In</span>
                     </button>
                     <button className='acitvebtn'>
-                        <span><i class="fa-solid fa-user-plus"></i></span>
+                        <span><i className="fa-solid fa-user-plus"></i></span>
                         <span>Sign Up</span>
                     </button>
                 </div>
                 <div className='inputDivSignup emailInputDiv'>
-                    <i class="fa-regular fa-envelope"></i>
+                    <i className="fa-regular fa-envelope"></i>
                     <input placeholder='Email' type="email" />
                 </div>
                 <div className='inputDivSignup UserNameInputDiv'>
-                    <i class="fa-regular fa-circle-user"></i>
+                    <i className="fa-regular fa-circle-user"></i>
                     <input placeholder='Username' type="text" />
                 </div>
                 <div className='inputDivSignup PassworInputDiv'>
-                    <i class="fa-solid fa-keyboard"></i>
+                    <i className="fa-solid fa-keyboard"></i>
                     <input placeholder='Password' type="password" />
                 </div>
                 <div className='inputDivSignup ConfirmInputDiv'>
-                    <i class="fa-regular fa-square-check"></i>
+                    <i className="fa-regular fa-square-check"></i>
                     <input placeholder='Confirm Password' type="password" />
                 </div>
                 <div className='AccepteTermsDiv ps-5 my-3'>
                     Accept <span style={{color: '#6f11f5', cursor: 'pointer', textDecoration: 'underline'}}>Terms & Conditions</span>
                 </div>
                 <div className='SignUpBtn mt-2'>
-                    <button><i class="fa-solid fa-user-plus me-1"></i> Sign up</button>
+                    <button><i className="fa-solid fa-user-plus me-1"></i> Sign up</button>
                 </div>
             </div>
         </div>
