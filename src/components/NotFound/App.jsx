4c263fd90f6d2e122df4a93390983bc0ed80610d
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-11-25 23:30:02 +0500

    kam chal rha hai

--- src/components/NotFound/App.jsx
+++ src/components/NotFound/App.jsx
@@ -6,11 +6,11 @@ function App(){
 
     return (
         <div className="d-flex p-5 m-5 flex-column">
-            <h1>404</h1>
+            <h1 className="h1">404</h1>
             <h3>Oops, you still haven't found what you looking for?</h3>
             <p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
             <div className="mt-5" onClick={() => navigatTo('/')}>
-                <button><i className="fa-solid fa-arrow-left"></i> Back to Home Page</button>
+                <button className="btnofnotfound"><i className="fa-solid fa-arrow-left"></i> Back to Home Page</button>
             </div>
         </div>
     )
