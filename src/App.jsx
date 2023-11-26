10a72363422e8603bd4fa451344496c5509075e2
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-11-26 20:45:21 +0500

    Sign up hogya hai

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Signup from './components/Signup/App.jsx'
 import Home from './components/Home/App.jsx'
 import Login from './components/Login/App.jsx'
+import Verifytoken from './components/Verifytoken/App.jsx'
 import NotFound from './components/NotFound/App.jsx'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="signup" element={<Signup/>}/>
         <Route path="login" element={<Login/>}/>
+        <Route path="verifytoken" element={<Verifytoken/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
---
+++ src/components/Verifytoken/App.jsx
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import myApi from "../../myApi.js";
+import { useLocation, useNavigate } from "react-router-dom";
+
+function App() {
+    const navigateTo = useNavigate();
+    const location = useLocation();
+
+    let [successTxt, setSuccessTxt] = useState('');
+    let [errTxt, setErrTxt] = useState('');
+
+    useEffect(() => {
+        let isMounted = true;  // Flag to track component mount status
+    
+        const fetchData = async () => {
+            const params = new URLSearchParams(location.search);
+            const tokensearch = params.get('token');
+    
+            try {
+                const res = await axios.post(`${myApi}verfiy-token`, { token: tokensearch });
+                const apiData = res.data;
+    
+                // Check if the component is still mounted before updating state
+                if (isMounted) {
+                    console.log(res.status, "res status");
+                    if (res.status === 200) {
+                        setSuccessTxt('Your account has been successfully completed');
+                        // console.log('Your account has been successfully completed');
+                    } else if (res.status === 401) {
+                        setErrTxt(apiData || 'Account already has been made');
+                        // console.log('Account already has been made');
+                    }
+                }
+            } catch (error) {
+                console.error(error.response.data);
+    
+                // Check if the component is still mounted before updating state
+                if (isMounted) {
+                    setErrTxt(error.response.data);
+                }
+            }
+        };
+    
+        fetchData();
+    
+        // Cleanup function to update the mounted status
+        return () => {
+            isMounted = false;
+        };
+    }, [location.search]);
+    
+
+    return (
+        <div className="container mt-5">
+            <div className="fs-3 text-danger">{errTxt}</div>
+            <div className="fs-3 text-success">{successTxt}</div>
+            <div className="mt-5" onClick={() => navigateTo('/login')}>
+                <button className="btnofnotfound"><i className="fa-solid fa-arrow-left"></i> Back to Login Page</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
